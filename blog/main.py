59fe542977aa3c1db9caeddadfaac35e36f4d095
Yahaya Hyellamoda Yohanna <hyohanna@bento.africa>
2021-05-12 18:59:16 +0100

    worked with response model

--- blog/main.py
+++ blog/main.py
@@ -22,16 +22,16 @@ def findOrFail(id: int, db: Session = Depends(get_db)):
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Blog with id: {id} not found")
-    return blog
+    return blog.first()
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=schemas.ShowBlog)
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return {"message": "All blogs fetched successfully", "data": blogs}
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id: int, db: Session = Depends(get_db)):
     blog = findOrFail(id, db)
     return {"message": "Blog fetched successfully", "data": blog}
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel
+from typing import Any
 
 
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class ShowBlog(BaseModel):
+    message: str
+    data: Any
+
+    class Config():
+        orm_mode = True
